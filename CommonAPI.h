af7d9a1362f572bc0070409be5ddf8873fcbb026
KJM <kjm99d@naver.com>
2023-03-18 02:40:43 +0900

    💡 Export Common API 주석 작성

--- CommonAPI.h
+++ CommonAPI.h
@@ -7,9 +7,9 @@
 #include "RegCtrl.h"
 
 #if WINSYSCOMMON_EXPORTS
-#define WINSYSCOMMON_API _declspec(dllexport) 
+#define WINSYSCOMMON_API _declspec(dllexport)
 #else
-#define WINSYSCOMMON_API _declspec(dllexport) 
+#define WINSYSCOMMON_API _declspec(dllexport)
 #endif
 
 #if __cplusplus
@@ -22,7 +22,25 @@ extern "C" {
 		typedef bool(*fp_GetRegistory)(HKEY hKey, wchar_t* subKey, wchar_t* valueName, wchar_t* pBuf, int pBufMaxLength);
 		typedef bool(*fp_SetRegistory)(HKEY hKey, wchar_t* subKey, wchar_t* valueName, wchar_t* data);
 
+		/**
+		 * GetRegistry는 지정된 레지스트리 키의 값을 검색합니다.
+		 * @param hKey 열린 레지스트리 키에 대한 핸들입니다.
+		 * @param subKey 검색할 레지스트리 하위 키의 이름입니다.
+		 * @param valueName 레지스트리 값의 이름입니다.
+		 * @param pBuf 지정된 키의 값이 들어갈 버퍼입니다.
+		 * @param pBufMaxLength 버퍼의 최대 크기입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
 		WINSYSCOMMON_API bool GetRegistry(HKEY hKey, wchar_t* subKey, wchar_t* valueName, wchar_t* pBuf, int pBufMaxLength);
+
+		/**
+		 * SetRegistry는 레지스트리 하위 키에 지정된 이름을 가진 값을 설정합니다.
+		 * @param hKey 열린 레지스트리 키에 대한 핸들입니다.
+		 * @param subKey 값을 설정할 레지스트리 하위 키의 이름입니다.
+		 * @param valueName 값을 설정할 레지스트리 값의 이름입니다.
+		 * @param data 저장할 데이터입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
 		WINSYSCOMMON_API bool SetRegistry(HKEY hKey, wchar_t* subKey, wchar_t* valueName, wchar_t* data);
 	}
 
@@ -36,15 +54,51 @@ extern "C" {
 		typedef BOOL(*fp_CopyFile)(char* strSrc, char* strDst);
 		typedef BOOL(*fp_GetFiles)(char* strSrc, std::vector<std::string>* files);
 
+		/**
+		 * CreateDirectory는 새 디렉터리를 만듭니다.
+		 * @param strPath 만들 디렉터리의 이름입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
 		WINSYSCOMMON_API BOOL CreateDirectory(char* strPath);
-		WINSYSCOMMON_API BOOL FileExist(char* strPath);
-		WINSYSCOMMON_API FileType GetType(char* strPath);
-		WINSYSCOMMON_API BOOL RemoveFIle(char* strPath);
-		WINSYSCOMMON_API BOOL CopyFile(char* strSrc, char* strDst);
-		WINSYSCOMMON_API BOOL GetFiles(char* strSrc, std::vector<std::string>* files);
 
+		/**
+		 * FileExist는 파일이 존재하는지 여부를 결정합니다.
+		 * @param strPath 테스트 할 파일 이름입니다.
+		 * @return 파일이 존재하는 경우 true를, 그렇지 않으면 false를 반환합니다.
+		 */
+		WINSYSCOMMON_API BOOL FileExist(char* strPath);
+
+		/**
+		 * GetType은 파일의 유형을 검색합니다.
+		 * @param strPath 검색 할 파일 이름입니다.
+		 * @return 파일의 유형입니다.
+		 */
+		WINSYSCOMMON_API FileType GetType(char* strPath);
+
+		/**
+		 * RemoveFIle은 파일을 삭제합니다.
+		 * @param strPath 삭제 할 파일의 이름입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
+		WINSYSCOMMON_API BOOL RemoveFIle(char* strPath);
+
+		/**
+		 * CopyFile은 기존 파일을 새 파일로 복사합니다.
+		 * @param strSrc 복사 할 기존 파일 이름입니다.
+		 * @param strDst 새 파일의 이름입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
+		WINSYSCOMMON_API BOOL CopyFile(char* strSrc, char* strDst);
+
+		/**
+		 * GetFiles는 지정된 디렉터리의 모든 파일 이름을 추출합니다.
+		 * @param strSrc 검색 할 디렉터리입니다.
+		 * @param files 파일 이름을 수신하는 벡터입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
+		WINSYSCOMMON_API BOOL GetFiles(char* strSrc, std::vector<std::string>* files);
 	}
-	
+
 	/**
 	 * 시스템 정보를 관리하는 API.
 	 */
@@ -53,11 +107,36 @@ extern "C" {
 		typedef BOOL(*fp_SetEnviroment)(char* key, char* value);
 		typedef BOOL(*fp_GetSystemTimeEx)(char* pBuf, int nBufSize);
 
-		WINSYSCOMMON_API BOOL GetEnviroment(char * key, char * pBuf, int nBufSize);
-		WINSYSCOMMON_API BOOL SetEnviroment(char * key, char *value);
-		WINSYSCOMMON_API BOOL GetSystemTimeEx(char* pBuf, int nBufSize);
-		WINSYSCOMMON_API BOOL IsProcessElevated();
+		/**
+		 * GetEnviroment는 지정된 환경 변수의 값을 검색합니다.
+		 * @param key 환경 변수의 이름입니다.
+		 * @param pBuf 지정된 환경 변수의 값을 받는 버퍼입니다.
+		 * @param nBufSize 버퍼의 최대 크기입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
+		WINSYSCOMMON_API BOOL GetEnviroment(char* key, char* pBuf, int nBufSize);
 
+		/**
+		 * SetEnviroment는 지정된 환경 변수의 값을 설정합니다.
+		 * @param key 환경 변수의 이름입니다.
+		 * @param value 환경 변수의 새 값입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
+		WINSYSCOMMON_API BOOL SetEnviroment(char* key, char* value);
+
+		/**
+		 * GetSystemTimeEx는 로컬 시간과 날짜를 검색합니다.
+		 * @param pBuf 시간 및 날짜를 받는 버퍼입니다.
+		 * @param nBufSize 버퍼의 최대 크기입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
+		WINSYSCOMMON_API BOOL GetSystemTimeEx(char* pBuf, int nBufSize);
+
+		/**
+		 * IsProcessElevated는 현재 프로세스가 관리자 권한으로 실행되고 있는지 여부를 결정합니다.
+		 * @return 현재 프로세스가 관리자 권한으로 실행되는 경우 true를, 그렇지 않으면 false를 반환합니다.
+		 */
+		WINSYSCOMMON_API BOOL IsProcessElevated();
 	}
 
 	/**
@@ -68,13 +147,44 @@ extern "C" {
 		typedef bool(*fp_StopProcess)(char*);
 		typedef bool(*fp_IsProcessRunning)(char*);
 		typedef bool(*fp_SetProcessPriority)(char*, DWORD);
-		typedef bool(*fp_MonitorProcessResources)(char*, PROCESS_RESOURCE_USAGE *);
+		typedef bool(*fp_MonitorProcessResources)(char*, PROCESS_RESOURCE_USAGE*);
 
+		/**
+		 * StartProcess는 새 프로세스를 시작합니다.
+		 * @param szPath 실행할 실행 파일의 경로입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
 		WINSYSCOMMON_API bool StartProcess(char* szPath);
+
+		/**
+		 * StopProcess는 프로세스를 이름으로 종료합니다.
+		 * @param szProcessName 종료할 프로세스의 이름입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
 		WINSYSCOMMON_API bool StopProcess(char* szProcessName);
+
+		/**
+		 * IsProcessRunning은 프로세스가 실행 중인지 여부를 결정합니다.
+		 * @param szProcessName 테스트할 프로세스의 이름입니다.
+		 * @return 프로세스가 실행 중이면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
 		WINSYSCOMMON_API bool IsProcessRunning(char* szProcessName);
+
+		/**
+		 * SetProcessPriority는 지정된 프로세스의 우선 순위 클래스를 설정합니다.
+		 * @param szProcName 프로세스의 이름입니다.
+		 * @param dwPriority 프로세스의 우선 순위 클래스입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
 		WINSYSCOMMON_API bool SetProcessPriority(char* szProcName, DWORD dwPriority);
-		WINSYSCOMMON_API bool MonitorProcessResources(char * szProcName, PROCESS_RESOURCE_USAGE * usage);
+
+		/**
+		 * MonitorProcessResources는 프로세스에서 사용하는 리소스에 대한 정보를 검색합니다.
+		 * @param szProcName 프로세스의 이름입니다.
+		 * @param usage 리소스 사용 정보를 수신하는 PROCESS_RESOURCE_USAGE 구조체에 대한 포인터입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
+		WINSYSCOMMON_API bool MonitorProcessResources(char* szProcName, PROCESS_RESOURCE_USAGE* usage);
 	}
 
 	/**
@@ -84,7 +194,24 @@ extern "C" {
 		typedef BOOL(*fp_AnsiStringToWideString)(char* pSrcBuf, int nSrcBufSize, wchar_t* pDstBuf, int pDstBufMaxSize);
 		typedef BOOL(*fp_WideStringToAnsiString)(wchar_t* pSrcBuf, int nSrcBufSize, char* pDstBuf, int pDstBufMaxSize);
 
+		/**
+		 * AnsiStringToWideString은 멀티바이트 문자열을 와이드 문자열로 변환합니다.
+		 * @param pSrcBuf 변환할 멀티바이트 문자열입니다.
+		 * @param nSrcBufSize 입력 버퍼의 최대 크기입니다.
+		 * @param pDstBuf 변환된 문자열을 받는 버퍼입니다.
+		 * @param pDstBufMaxSize 출력 버퍼의 최대 크기입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
 		WINSYSCOMMON_API BOOL AnsiStringToWideString(char* pSrcBuf, int nSrcBufSize, wchar_t* pDstBuf, int pDstBufMaxSize);
+
+		/**
+		 * WideStringToAnsiString은 와이드 문자열을 멀티바이트 문자열로 변환합니다.
+		 * @param pSrcBuf 변환할 와이드 문자열입니다.
+		 * @param nSrcBufSize 입력 버퍼의 최대 크기입니다.
+		 * @param pDstBuf 변환된 문자열을 받는 버퍼입니다.
+		 * @param pDstBufMaxSize 출력 버퍼의 최대 크기입니다.
+		 * @return 함수가 성공하면 true를, 그렇지 않으면 false를 반환합니다.
+		 */
 		WINSYSCOMMON_API BOOL WideStringToAnsiString(wchar_t* pSrcBuf, int nSrcBufSize, char* pDstBuf, int pDstBufMaxSize);
 
 	}
@@ -92,5 +219,3 @@ extern "C" {
 #if __cplusplus
 }
 #endif
-
-
--- RegCtrl.h
+++ RegCtrl.h
@@ -11,15 +11,61 @@ extern CRegCtrl RegCtrl;
 class CRegCtrl
 {
 public:
-    CRegCtrl() = default;
-    ~CRegCtrl() = default;
-
+    /**
+     * @brief 레지스트리에 값을 설정합니다.
+     * @param hKey 레지스트리 키
+     * @param subKey 서브 키
+     * @param valueName 값 이름
+     * @param data 설정할 데이터
+     * @return 설정 성공 여부
+     */
     bool SetRegistry(HKEY hKey, const std::wstring& subKey, const std::wstring& valueName, const std::wstring& data);
+
+    /**
+     * @brief 레지스트리에서 값을 가져옵니다.
+     * @param hKey 레지스트리 키
+     * @param subKey 서브 키
+     * @param valueName 값 이름
+     * @param data 가져온 데이터
+     * @return 가져오기 성공 여부
+     */
     bool GetRegistry(HKEY hKey, const std::wstring& subKey, const std::wstring& valueName, std::wstring& data);
 
 private:
+    /**
+     * @brief 레지스트리 키 열기
+     * @param hKey 레지스트리 키
+     * @param subKey 서브 키
+     * @param access 레지스트리 접근 권한
+     * @param resultKey 결과 키
+     * @return 열기 성공 여부
+     */
     bool OpenRegistryKey(HKEY hKey, const std::wstring& subKey, REGSAM access, HKEY& resultKey);
+
+    /**
+     * @brief 값을 쿼리합니다.
+     * @param resultKey 결과 키
+     * @param valueName 값 이름
+     * @param data 가져온 데이터
+     * @param dataSize 가져온 데이터 크기
+     * @return 쿼리 성공 여부
+     */
     bool QueryValue(HKEY resultKey, const std::wstring& valueName, std::wstring& data, DWORD& dataSize);
+
+    /**
+     * @brief 값을 설정합니다.
+     * @param resultKey 결과 키
+     * @param valueName 값 이름
+     * @param data 설정할 데이터
+     * @param dataSize 설정할 데이터 크기
+     * @return 설정 성공 여부
+     */
     bool SetValue(HKEY resultKey, const std::wstring& valueName, const std::wstring& data, DWORD dataSize);
+
+    /**
+     * @brief 레지스트리 키 닫기
+     * @param hKey 레지스트리 키
+     * @return 닫기 성공 여부
+     */
     bool CloseRegistryKey(HKEY hKey);
 };
